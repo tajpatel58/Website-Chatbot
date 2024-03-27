8fe163fb3c05fea548b9ee5697df0c46fb5172e9
Taj Patel <tajpatel58@gmail.com>
2024-03-27 06:47:55 +0000

    adding removal of stop words from text

--- Scripts/text_cleaning.py
+++ Scripts/text_cleaning.py
@@ -1,11 +1,13 @@
-### Import Packages:
 import torch
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
-def clean_text(text, stemmer):
+
+def clean_text(text: str, stemmer: PorterStemmer, stop_words: list) -> list:
     lowered_text = text.lower()
-    list_text = lowered_text.split(' ')
-    clean_text = [stemmer.stem(word) for word in list_text]
-    return clean_text 
+    list_text = lowered_text.split(" ")
+    clean_text = [stemmer.stem(word) for word in list_text if word not in stop_words]
+    return clean_text
 
 
 def bag_of_words(tokenized_text, bag):
