71b81761ae2dcd488eca8a494fd2f648aa90d2c6
Taj Patel <tajpatel58@gmail.com>
2024-05-03 00:44:17 +0100

    refactoring training script

--- src/scripts/training.py
+++ src/scripts/training.py
@@ -8,16 +8,15 @@ from torch.optim import Adam
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from torch.utils.data import DataLoader
-from src.scripts.models.model import NeuralNet
+from src.scripts.models.neural_net import NeuralNet
 from src.scripts.data.dataset import ChatbotDataset
 from torch.utils.tensorboard import SummaryWriter
 nltk.download('stopwords')
 
 # Create Writer:
 writer = SummaryWriter("/Users/taj/Documents/Website-Chatbot/logging/tensorboard")
-mlflow.set_tracking_uri("sqlite:////Users/taj/Documents/Website-Chatbot/logging/mlflow.db")
+mlflow.set_tracking_uri("http://127.0.0.1:6090")
 mlflow.set_experiment("chatbot")
-mlflow.pytorch.autolog()
 
 # Data Processing Variables:
 data_path = "/Users/taj/Documents/Website-Chatbot/data/message_data.json"
@@ -36,6 +35,7 @@ num_classes = train_data.get_num_classes()
 batch_size = 6
 learning_rate = 0.001
 num_epochs = 1000
+num_training_data_points = len(train_data)
 
 # Create Dataloader to pass data in batches:
 train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
@@ -69,19 +69,23 @@ def train(model, optim, loss_func, number_of_epochs=100):
             error.backward()
             optim.step()
             optim.zero_grad()
-
+            
+        epoch_accuracy = torch.div(epoch_accuracy, num_training_data_points)
         if epoch_accuracy >= best_acc:
             best_model = model_state
+            best_acc = epoch_accuracy
 
     # Load the model with the highest testing accuracy
     model.load_state_dict(best_model)
 
     writer.flush()
 
-    return model
+    return model, best_acc
 
 
-trained_model = train(nn_model, optimizer, loss, number_of_epochs=1000)
+trained_model, best_accuracy = train(nn_model, optimizer, loss, number_of_epochs=1000)
+mlflow.pytorch.log_model(trained_model, "model")
+mlflow.log_metric("training_loss", best_accuracy)
 
 # Once we complete the training loop, we want to store some of the parameters like the "bag" so that once the bot is built,
 # and a new message comes in, this can be turned into a feature vector and passed through our model.
